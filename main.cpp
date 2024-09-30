ae9417bc0a40b6d40517d23b7ab9f10506b89fd4
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-09-29 17:26:28 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Avleen Gill - 210 Lab 15
+
+#include <iostream>
+using namespace std;
+
+class Movie {
+    private:
+    string title; //movie title
+    int year; //year released
+    string sw; //screenwriter
+
+    public:
+};
+
+int main() {
+
+}
