63d77623c16eef4ee2874f9a19a6c371c430274e
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-09-29 17:37:47 -0700

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 //Avleen Gill - 210 Lab 15
 
 #include <iostream>
+#include <array>
+#include <fstream>
 using namespace std;
 
 class Movie {
@@ -10,6 +12,13 @@ class Movie {
     string sw; //screenwriter
 
     public:
+    string getTitle() const         { return title; }
+    int getYear() const             { return year; }
+    string getSW() const            { return sw; }
+    string setTitle(string t)       { title = t; }
+    int setYear(int y)              { year = y; }
+    string setSW(string s)          { sw = s; }
+
 };
 
 int main() {
